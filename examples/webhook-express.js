f397e0cc3e415c92b3edc45230794a607ab463d6
Mikhail Semin <bifot@bifot.ru>
2018-11-22 19:16:47 +0300

    added base support for webhooks

---
+++ examples/webhook-express.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const VkBot = require('../lib')
+
+const app = express()
+const bot = new VkBot({
+  token: process.env.TOKEN,
+  confirmation: process.env.CONFIRMATION,
+})
+
+bot.on((ctx) => {
+  ctx.reply('Hello!')
+})
+
+app.use(bodyParser.json())
+
+app.post('/', bot.webhookCallback)
+
+app.listen(process.env.PORT)
