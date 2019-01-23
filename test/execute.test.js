ca8f4e4ac1a06255faa6c324558cab7e662a97a0
Mikhail Semin <bifot@bifot.ru>
2019-01-23 16:55:41 +0300

    fixed callback execute solution

--- test/execute.test.js
+++ test/execute.test.js
@@ -14,7 +14,9 @@ describe('execute', () => {
   });
 
   it('should execute one request (callback)', (done) => {
-    bot.execute('users.get', { user_id: 145003487 }, (response) => {
+    bot.execute('users.get', { user_id: 145003487 }, (err, response) => {
+      expect(err).to.be.equal(null);
+
       expect(response).to.be.an('object');
       expect(response.id).to.be.a('number');
       expect(response.first_name).to.be.a('string');
@@ -40,11 +42,13 @@ describe('execute', () => {
   it('should execute 75 requests with one failed (callback)', (done) => {
     Array(75).fill(null).map(() => bot.execute('users.get', { user_id: 145003487 }));
 
-    bot.execute('wall.post', {}, (err) => {
+    bot.execute('wall.post', {}, (err, response) => {
       expect(err).to.be.an('object');
       expect(err.method).to.be.a('string');
       expect(err.error_code).to.be.a('number');
       expect(err.error_msg).to.be.a('string');
+
+      expect(response).to.be.equal(null);
 
       done();
     });
